2304dda6d2896c38079325b9ec59b0efdb424464
Asvan-Launcher <95467525+Amzi-kwasi@users.noreply.github.com>
2026-03-23 20:39:53 +0000

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,19 +1,17 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBxHUxPexhKi5k7Xt7yPXpU4H-lVg9p9xY",
+  authDomain: "asvan-studio-a8d03.firebaseapp.com",
+  projectId: "asvan-studio-a8d03",
+  storageBucket: "asvan-studio-a8d03.firebasestorage.app",
+  messagingSenderId: "433330962153",
+  appId: "1:433330962153:web:4332da63652b72d0f07a78"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
-export default app;
