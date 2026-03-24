14e4110f9dc665bef7892121902dff35fc4ca888
Asvan-Launcher <95467525+Amzi-kwasi@users.noreply.github.com>
2026-03-24 12:30:56 +0000

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,17 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBxHUxPexhKi5k7Xt7yPXpU4H-lVg9p9xY",
-  authDomain: "asvan-studio-a8d03.firebaseapp.com",
-  projectId: "asvan-studio-a8d03",
-  storageBucket: "asvan-studio-a8d03.firebasestorage.app",
-  messagingSenderId: "433330962153",
-  appId: "1:433330962153:web:4332da63652b72d0f07a78"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export default app;
